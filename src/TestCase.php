710a9be527604b5435533f91dc950d1422e54d9a
John Blackbourn <john@johnblackbourn.com>
2016-07-16 13:33:28 +0100

    Move error strings into properties, ready for re-use in tests.

--- src/TestCase.php
+++ src/TestCase.php
@@ -3,6 +3,20 @@ namespace Johnbillion\DocsStandards;
 
 abstract class TestCase extends \PHPUnit_Framework_TestCase {
 
+	public static $docblock_missing                  = 'The docblock for `%s` should not be missing.';
+	public static $docblock_desc_empty               = 'The docblock description for `%s` should not be empty.';
+	public static $param_count_mismatch              = 'The number of @param docs for `%s` should match its number of parameters.';
+	public static $param_desc_empty                  = 'The @param description for the `%s` parameter of `%s` should not be empty.';
+	public static $param_name_incorrect              = 'The @param name for the `%s` parameter of `%s` is incorrect.';
+	public static $param_type_hint_accept_array      = 'The @param type hint for the `%s` parameter of `%s` should state that it accepts an array.';
+	public static $param_type_hint_accept_object     = 'The @param type hint for the `%s` parameter of `%s` should state that it accepts an object of type `%s`.';
+	public static $param_type_hint_disallow_callback = '`callback` is not a valid type in the @param type hint for the `%s` parameter of `%s`. `callable` should be used instead.';
+	public static $param_type_hint_accept_callable   = 'The @param type hint for the `%s` parameter of `%s` should state that it accepts a callable.';
+	public static $param_type_hint_optional          = 'The @param description for the optional `%s` parameter of `%s` should state that it is optional.';
+	public static $param_type_hint_not_optional      = 'The @param description for the required `%s` parameter of `%s` should not state that it is optional.';
+	public static $param_type_hint_default           = 'The @param description for the `%s` parameter of `%s` should state its default value.';
+	public static $param_type_hint_no_default        = 'The @param description for the `%s` parameter of `%s` should not state a default value.';
+
 	abstract protected function getTestFunctions();
 
 	abstract protected function getTestClasses();
@@ -33,21 +47,22 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase {
 		$doc_params    = $docblock->getTagsByName( 'param' );
 
 		$this->assertNotFalse( $doc_comment, sprintf(
-			'The docblock for `%s` should not be missing.',
+			self::$docblock_missing,
 			$name
 		) );
 
 		$this->assertNotEmpty( $docblock->getShortDescription(), sprintf(
-			'The docblock description for `%s` should not be empty.',
+			self::$docblock_desc_empty,
 			$name
 		) );
 
 		$this->assertSame( count( $method_params ), count( $doc_params ), sprintf(
-			'The number of @param docs for `%s` should match its number of parameters.',
+			self::$param_count_mismatch,
 			$name
 		) );
 
 		// @TODO check description ends in full stop
+		// @TODO tests for @link or @see in descriptions, etc
 
 		foreach ( $method_params as $i => $param ) {
 
@@ -66,7 +81,7 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase {
 			}
 
 			$this->assertNotEmpty( $description, sprintf(
-				'The @param description for the `%s` parameter of `%s` should not be empty.',
+				self::$param_desc_empty,
 				$param_doc->getVariableName(),
 				$name
 			) );
@@ -74,14 +89,14 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase {
 			list( $param_doc_type, $param_doc_name ) = preg_split( '#\s+#', $param_doc->getContent() );
 
 			$this->assertSame( '$' . $param->getName(), $param_doc_name, sprintf(
-				'The @param name for the `%s` parameter of `%s` is incorrect.',
+				self::$param_name_incorrect,
 				'$' . $param->getName(),
 				$name
 			) );
 
 			if ( $param->isArray() ) {
 				$this->assertNotFalse( strpos( $param_doc_type, 'array' ), sprintf(
-					'The @param type hint for the `%s` parameter of `%s` should state that it accepts an array.',
+					self::$param_type_hint_accept_array,
 					$param_doc->getVariableName(),
 					$name
 				) );
@@ -89,7 +104,7 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase {
 
 			if ( ( $param_class = $param->getClass() ) && ( 'stdClass' !== $param_class->getName() ) ) {
 				$this->assertNotFalse( strpos( $param_doc_type, $param_class->getName() ), sprintf(
-					'The @param type hint for the `%s` parameter of `%s` should state that it accepts an object of type `%s`.',
+					self::$param_type_hint_accept_object,
 					$param_doc->getVariableName(),
 					$name,
 					$param_class->getName()
@@ -97,14 +112,14 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase {
 			}
 
 			$this->assertFalse( strpos( $param_doc_type, 'callback' ), sprintf(
-				'`callback` is not a valid type in the @param type hint for the `%s` parameter of `%s`. `callable` should be used instead.',
+				self::$param_type_hint_disallow_callback,
 				$param_doc->getVariableName(),
 				$name
 			) );
 
 			if ( $param->isCallable() ) {
 				$this->assertNotFalse( strpos( $param_doc_type, 'callable' ), sprintf(
-					'The @param type hint for the `%s` parameter of `%s` should state that it accepts a callable.',
+					self::$param_type_hint_accept_callable,
 					$param_doc->getVariableName(),
 					$name
 				) );
@@ -112,13 +127,13 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase {
 
 			if ( $param->isOptional() ) {
 				$this->assertNotFalse( strpos( $description, 'Optional.' ), sprintf(
-					'The @param description for the optional `%s` parameter of `%s` should state that it is optional.',
+					self::$param_type_hint_optional,
 					$param_doc->getVariableName(),
 					$name
 				) );
 			} else {
 				$this->assertFalse( strpos( $description, 'Optional.' ), sprintf(
-					'The @param description for the required `%s` parameter of `%s` should not state that it is optional.',
+					self::$param_type_hint_not_optional,
 					$param_doc->getVariableName(),
 					$name
 				) );
@@ -126,13 +141,13 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase {
 
 			if ( $param->isDefaultValueAvailable() && ( array() !== $param->getDefaultValue() ) ) {
 				$this->assertNotFalse( strpos( $description, 'Default ' ), sprintf(
-					'The @param description for the `%s` parameter of `%s` should state its default value.',
+					self::$param_type_hint_default,
 					$param_doc->getVariableName(),
 					$name
 				) );
 			} else {
 				$this->assertFalse( strpos( $description, 'Default ' ), sprintf(
-					'The @param description for the `%s` parameter of `%s` should not state a default value.',
+					self::$param_type_hint_no_default,
 					$param_doc->getVariableName(),
 					$name
 				) );
