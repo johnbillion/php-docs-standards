79d71f367555901c2b1f41ef17413c3489024531
John Blackbourn <john@johnblackbourn.com>
2016-07-16 19:28:54 +0100

    Dogfood our own classes, including the test classes.

---
+++ tests/test-dogfood.php
@@ -0,0 +1,31 @@
+<?php
+namespace Johnbillion\DocsStandards\Tests;
+
+/**
+ * Test the tests that test the tests.
+ */
+class Dogfood extends \Johnbillion\DocsStandards\TestCase {
+
+	/**
+	 * Return an array of function names that will be through the test suite.
+	 *
+	 * @return array Function names to test.
+	 */
+	protected function getTestFunctions() {
+		return array();
+	}
+
+	/**
+	 * Return an array of class names whose methods will be run through the test suite.
+	 *
+	 * @return array Class names to test.
+	 */
+	protected function getTestClasses() {
+		return array(
+			'\Johnbillion\DocsStandards\TestCase',
+			__NAMESPACE__ . '\Docblock',
+			__NAMESPACE__ . '\Dogfood',
+		);
+	}
+
+}
